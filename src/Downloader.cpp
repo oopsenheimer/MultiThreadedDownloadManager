5705070c5dc948ffd1d9add15b2131744a388b9c
oopsenheimer <anujverma3563@gmail.com>
2026-03-14 00:53:22 +0530

    added capabitlity to get the extension of the file

--- src/Downloader.cpp
+++ src/Downloader.cpp
@@ -7,36 +7,82 @@
 #include <cstddef>
 #include <fstream>
 #include <ios>
+#include <iostream>
+#include <regex>
 #include <string>
 
-std::size_t Downloader::get_download_file_size() {
-    auto handle = curl_easy_init();
-    if (handle == nullptr)
-        return 0;
-
-    curl_easy_setopt(handle, CURLOPT_URL, target_url.c_str());
-    curl_easy_setopt(handle, CURLOPT_NOBODY, 1L);
-    curl_easy_setopt(handle, CURLOPT_FAILONERROR, 1);
-
-    CURLcode result = curl_easy_perform(handle);
-    if (result != CURLE_OK) {
-        curl_easy_cleanup(handle);
-        return 0;
-    }
-
-    curl_off_t file_size;
-    curl_easy_getinfo(handle, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &file_size);
-    curl_easy_cleanup(handle);
-    return static_cast<std::size_t>(file_size);
+std::string Downloader::get_file_name() {
+    if (!headers_fetched)
+        fetch_headers();
+    return file_name;
 }
 
-bool Downloader::download_chunk(int thread_id, std::size_t start_byte, std::size_t end_byte) {
-    std::ofstream file(FILE_NAME, std::ios::binary | std::ios::in | std::ios::out);
+std::size_t Downloader::get_file_size() {
+    if (!headers_fetched)
+        fetch_headers();
+    return file_size;
+}
+
+void Downloader::fetch_headers() {
+    auto handle = curl_easy_init();
+    if (handle == nullptr)
+        return;
+
+    curl_easy_setopt(handle, CURLOPT_URL, target_url.c_str());
+    curl_easy_setopt(handle, CURLOPT_NOBODY, 1);
+
+    curl_easy_setopt(handle, CURLOPT_HEADERFUNCTION, header_call_back);
+    curl_easy_setopt(handle, CURLOPT_HEADERDATA, &file_name);
+
+    auto result = curl_easy_perform(handle);
+
+    if (result == CURLE_OK) {
+        curl_off_t size;
+        curl_easy_getinfo(handle, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &size);
+        file_size = static_cast<std::size_t>(size);
+    } else {
+        std::cerr << "Failed to fetch headers: " << curl_easy_strerror(result) << std::endl;
+    }
+
+    curl_easy_cleanup(handle);
+    headers_fetched = true;
+}
+
+std::size_t Downloader::header_call_back(char* buffer, std::size_t size, std::size_t nmemb,
+                                         void* stream) {
+    std::size_t total_size = size * nmemb;
+    std::string header_line(buffer, total_size);
+    auto file_stream = static_cast<std::string*>(stream);
+
+    std::regex pattern("Content-Type:\\s*([^\\s]+)", std::regex_constants::icase);
+    std::smatch matches;
+
+    if (std::regex_search(header_line, matches, pattern)) {
+        std::string mime_type = matches[1].str();
+        
+        if (mime_type == "application/pdf") {
+            *file_stream = "download.pdf";
+        } 
+        else if (mime_type == "image/jpeg") {
+            *file_stream = "download.jpg";
+        }
+        else if (mime_type == "text/html") {
+            *file_stream = "download.html";
+        }
+
+    }
+    std::cout << header_line << std::endl;
+    return total_size;
+}
+
+void Downloader::download_chunk(int thread_id, std::size_t start_byte, std::size_t end_byte,
+                                const std::string& output_file_name) {
+    std::ofstream file(output_file_name, std::ios::binary | std::ios::in | std::ios::out);
     file.seekp(static_cast<std::streamoff>(start_byte));
 
     auto handle = curl_easy_init();
     if (handle == nullptr)
-        return false;
+        return;
 
     auto range = std::to_string(start_byte) + "-" + std::to_string(end_byte);
 
@@ -46,14 +92,11 @@ bool Downloader::download_chunk(int thread_id, std::size_t start_byte, std::size
     curl_easy_setopt(handle, CURLOPT_RANGE, range.c_str());
 
     CURLcode result = curl_easy_perform(handle);
-
-    curl_easy_cleanup(handle);
-
     if (result != CURLE_OK) {
-        return false;
+        std::cerr << "Thread " << thread_id << " failed: " << curl_easy_strerror(result)
+                  << std::endl;
     }
-
-    return true;
+    curl_easy_cleanup(handle);
 }
 
 std::size_t Downloader::write_call_back(void* buffer, std::size_t size, std::size_t nmemb,
--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,9 @@ auto num_threads = std::thread::hardware_concurrency();
 
 int main() {
     Downloader downloadManager(TEST_URL);
-    auto file_size = downloadManager.get_download_file_size();
-    Downloader::prepare_write_stream("out.bin", file_size);
+    auto file_size = downloadManager.get_file_size();
+    auto file_name = downloadManager.get_file_name();
+    Downloader::prepare_write_stream(file_name, file_size);
     std::cout << "Size of " << TEST_URL << " = " << file_size << std::endl;
 
     std::vector<std::thread> workers;
@@ -27,14 +28,11 @@ int main() {
             end_byte = file_size - 1;
         else
             end_byte = start_byte + diff - 1;
-        workers.emplace_back(&Downloader::download_chunk, &downloadManager, i, start_byte, end_byte);
+        workers.emplace_back(&Downloader::download_chunk, &downloadManager, i, start_byte, end_byte, downloadManager.get_file_name());
         start_byte += diff;
     }
 
     for (auto& worker : workers) {
         worker.join();
     }
-
-
-    // downloadManager.download_chunk(1, 0, 500);
 }
